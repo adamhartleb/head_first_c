63c3d6a654af97507ca89e47c9f72e82345d2580
Adam Hartleb <adamjhartleb@gmail.com>
2019-03-26 14:23:14 -0700

    Worked with malloc and free

--- ch6/linkedlist.c
+++ ch6/linkedlist.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct island {
     char *name;
@@ -14,6 +16,18 @@ void display(island *start)
     for (;i != NULL; i = i->next) {
         printf("Name: %s open: %s-%s\n", i->name, i->opens, i->closes);
     }
+}
+
+island* create(char *name)
+{
+    island *i = malloc(sizeof(island));
+
+    i->name = strdup(name);
+    i->opens = "09:00";
+    i->closes = "17:00";
+    i->next = NULL;
+
+    return i;
 }
 
 int main()
